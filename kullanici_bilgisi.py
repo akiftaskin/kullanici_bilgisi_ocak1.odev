2c99b908fcce7d0305c0fc81a3812a1873cf6988
akiftaskin <akiftaskin52@gmail.com>
2025-03-11 15:45:35 +0300

    Add files via upload

---
+++ kullanici_bilgisi.py
@@ -0,0 +1,7 @@
+# Kullanıcıdan giriş al
+ad = input("Adınızı girin: ")
+soyad = input("Soyadınızı girin: ")
+yas = input("Yaşınızı girin: ")
+
+# Çıktıyı istenilen formatta göster
+print(f"Merhaba {ad} {soyad}, {yas} yaşındasınız. Hoş geldiniz!")
